55e08eed83a9d7f1167f1b4319293fb9b1426a12
aabathorat11 <aabathorat11@gmail.com>
2026-05-02 18:01:55 +0530

    Update data.tf

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "quantamvector-infra-statefile-backup"
+    bucket = "quantamvector-infra-statefile-backups"
     key    = "quantamvector/1-network/terraform.tfstate"
     region = "ap-northeast-1"
   }

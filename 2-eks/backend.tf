b0016e70e1448114188a9e69b695f0038f2ac8e6
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-05-05 16:59:54 +0530

    latest chnages

--- 2-eks/backend.tf
+++ 2-eks/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "laxmanraju-state-lock"
     key            = "laxmanraju/2-eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "laxmanraju-state-DB-1-lock"
+   // dynamodb_table = "laxmanraju-state-DB-1-lock"
+    use_lockfile  = true   # replaces dynamodb_table
     encrypt        = true
   }
 }

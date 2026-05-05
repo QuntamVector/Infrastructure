76a314bdc77a83897002684747ca6c34aed40834
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-05-05 15:20:50 +0530

    latest chnages

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
 }
 
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = "laxmanraju-state-2-lock"
+  name         = "laxmanraju-state-DB-1-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 

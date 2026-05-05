f2a1e5f3d07e21958dc78b92597042a03e81a84e
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-05-05 18:55:30 +0530

    latets chnages

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "laxmanraju-state-lock"
+  bucket = "laxmanraju-statefile-logs"
 }
 
 resource "aws_s3_bucket_versioning" "tf_state" {
   bucket = aws_s3_bucket.tf_state.id
-
+ 
   versioning_configuration {
     status = "Enabled"
   }

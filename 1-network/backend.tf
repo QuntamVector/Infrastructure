7b51465a28558c01e4e3657f3fa9282bbf31b9ce
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-05-05 16:31:58 +0530

    latest chnages

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -1,9 +1,19 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "laxmanraju-state-lock"
+#     key            = "laxmanraju/1-network/terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "laxmanraju-state-DB-1-lock"
+#     encrypt        = true
+#   }
+# }
+
 terraform {
   backend "s3" {
-    bucket         = "laxmanraju-state-lock"
-    key            = "laxmanraju/1-network/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "laxmanraju-state-DB-1-lock"
-    encrypt        = true
+    bucket        = "laxmanraju-state-lock"   # your S3 bucket
+    key           = "env/terraform.tfstate"   # path inside the bucket
+    region        = "us-east-1"
+    encrypt       = true
+    use_lockfile  = true                      # replaces dynamodb_table
   }
 }

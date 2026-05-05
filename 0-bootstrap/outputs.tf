e6a765759a5a5a0bfa93f307e5c35514b862652b
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-05-05 16:37:31 +0530

    latest chnages

--- 0-bootstrap/outputs.tf
+++ 0-bootstrap/outputs.tf
@@ -2,6 +2,6 @@ output "tf_state_bucket" {
   value = aws_s3_bucket.tf_state.bucket
 }
 
-output "tf_lock_table" {
-  value = aws_dynamodb_table.tf_lock.name
-}
+# output "tf_lock_table" {
+#   value = aws_dynamodb_table.tf_lock.name
+# }

22513dcea8b0e9bbac41c962b7e5c4868c195583
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-05-05 19:22:52 +0530

    latest chnages

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "laxmanraju-state-lock"
+    bucket = "laxmanraju-statefile-logs"
     key    = "laxmanraju/1-network/terraform.tfstate"
     region = "us-east-1"
   }

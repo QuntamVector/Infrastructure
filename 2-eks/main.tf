05fff5a8dd73546f385a41eb4672e9678c92cc42
ManojKRISHNAPPA <contactmanojmech@gmail.com>
2026-08-20 20:34:12 +0530

    updated the module types

--- 2-eks/main.tf
+++ 2-eks/main.tf
@@ -8,35 +8,39 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.25.0"
 
-  cluster_name    = var.project
-  cluster_version = var.kubernetes_version
+  # EKS Cluster
+  name               = var.project
+  kubernetes_version = var.kubernetes_version
 
   vpc_id     = local.vpc_id
   subnet_ids = local.private_subnet_ids
 
-  # Enable IAM Roles for Service Accounts (IRSA) - required for ALB controller later
+  # Enable IAM Roles for Service Accounts (IRSA)
   enable_irsa = true
 
-  # Endpoint access
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-  enable_cluster_creator_admin_permissions = true
+  # Cluster Endpoint Access
+  endpoint_public_access  = true
+  endpoint_private_access = true
 
+  # Give the IAM identity running Terraform admin access to the cluster
+  enable_cluster_creator_admin_permissions = true
 
   # Managed Node Group(s)
   eks_managed_node_groups = {
     default = {
       name           = "${var.project}-ng"
-      instance_types = ["t2.medium"]
-      min_size       = 3
-      max_size       = 5
-      desired_size   = 3
+      instance_types = ["t3.medium"]
+
+      min_size     = 3
+      max_size     = 5
+      desired_size = 3
 
       subnet_ids = local.private_subnet_ids
 
-      # Disable launch template so remote_access (SSH) can be used
+      # Disable custom launch template so remote_access can be used
       use_custom_launch_template = false
 
+      # SSH access to worker nodes
       remote_access = {
         ec2_ssh_key               = var.ssh_key_name
         source_security_group_ids = []
@@ -44,7 +48,9 @@ module "eks" {
     }
   }
 
+  # Common Tags
   tags = {
     Project = var.project
   }
 }
+
